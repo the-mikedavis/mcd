1dceddee24bf9c5052cc13948b08859ff8040170
Michael Davis <mcd164@miami.edu>
2018-03-04 00:19:34 -0500

    Format the barchart

--- assets/js/barchart.js
+++ assets/js/barchart.js
@@ -1,51 +1,51 @@
 export default function () {
-    const svg = d3.select('svg#barchart'),
-        margin = window.margin || {left: 40, right: 20, top: 20, bottom: 30};
+  const svg = d3.select('svg#barchart'),
+    margin = window.margin || {left: 40, right: 20, top: 20, bottom: 30};
 
-    let height = svg.node().getBoundingClientRect().width,
-        width;
+  let height = svg.node().getBoundingClientRect().width,
+    width;
 
-    svg.attr('height', height);
+  svg.attr('height', height);
 
-    width = height - margin.right - margin.left;
-    height = height - margin.top - margin.bottom;
+  width = height - margin.right - margin.left;
+  height = height - margin.top - margin.bottom;
 
-    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y = d3.scaleLinear().rangeRound([height, 0]),
-        g = svg.append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+  const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]),
+    g = svg.append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    d3.json('/json/linguae.json', function (data) {
-        const largest = Math.max.apply(Math, data.map(function (e) {
-            return e.time;
-        }));
+  d3.json('/json/linguae.json', function (data) {
+    const largest = Math.max.apply(Math, data.map(function (e) {
+      return e.time;
+    }));
 
-        x.domain(data.map(function (e) {
-            return width > 500 ? e.name : e.alias || e.name;
-        }));
-        y.domain([0, largest]);
+    x.domain(data.map(function (e) {
+      return width > 500 ? e.name : e.alias || e.name;
+    }));
+    y.domain([0, largest]);
 
-        g.append('g')
-            .attr('class', 'axis xaxis')
-            .attr('transform', `translate(0,${height})`)
-            .call(d3.axisBottom(x));
+    g.append('g')
+      .attr('class', 'axis xaxis')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x));
 
-        g.append('g')
-            .attr('class', 'axis yaxis')
-            .call(d3.axisLeft(y).ticks(5));
+    g.append('g')
+      .attr('class', 'axis yaxis')
+      .call(d3.axisLeft(y).ticks(5));
 
-        g.selectAll('rect.bar')
-            .data(data)
-            .enter()
-            .append('rect')
-            .classed('bar', true)
-            .attr('x', d => x(width > 500 ? d.name : d.alias || d.name))
-            .attr('y', d => y(d.time))
-            .attr('width', x.bandwidth())
-            .attr('height', d => height - y(d.time))
-            .attr('stroke', '#000')
-            .attr('stroke-width', 1)
-            .style('fill', d => d.color || "#000");
+    g.selectAll('rect.bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('x', d => x(width > 500 ? d.name : d.alias || d.name))
+      .attr('y', d => y(d.time))
+      .attr('width', x.bandwidth())
+      .attr('height', d => height - y(d.time))
+      .attr('stroke', '#000')
+      .attr('stroke-width', 1)
+      .style('fill', d => d.color || "#000");
 
-    });
+  });
 }
