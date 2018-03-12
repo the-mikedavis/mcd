c164a97fda069cf961bd46c99c5da5a20f42b8fd
Michael Davis <mcd164@miami.edu>
2018-03-12 11:34:43 -0400

    Ignore non-markdown in post crawler

--- lib/mcd/content/crawler.ex
+++ lib/mcd/content/crawler.ex
@@ -1,8 +1,8 @@
 defmodule Mcd.Content.Crawler do
   alias Mcd.Content.Post
 
-  def crawl do
-    File.ls!("priv/posts")
+  def crawl() do
+    find()
     |> Enum.map(fn(file) -> Task.async(fn -> Post.compile(file) end) end)
     |> Enum.map(&Task.await/1)
     |> Enum.sort(&sort/2)
@@ -10,5 +10,10 @@ defmodule Mcd.Content.Crawler do
 
   def sort(a, b) do
     Time.compare(a.date, b.date) > 0
+  end
+
+  def find() do
+    File.ls!("priv/posts")
+    |> Enum.filter(&(Regex.run(~r/.*\.md$/, &1)))
   end
 end
