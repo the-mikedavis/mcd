39bf3f50352a73b2864506a293037a987edc7721
Michael Davis <mcd164@miami.edu>
2018-05-14 21:20:42 +0000

    Small prod changes

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mcd, McdWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  http: [port: 4000],
+  url: [host: "mcarsondavis.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
